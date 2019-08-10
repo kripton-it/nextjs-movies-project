31950b8e17a7e4bc16b2a93d073d7956d8f8d847
Mikhail Shardin <kriptonit1988@gmail.com>
2019-08-10 17:16:09 +0500

    Альтернатива <a> внутри Link

--- pages/index.js
+++ pages/index.js
@@ -3,13 +3,13 @@ import Link from "next/link";
 const Blablabla = () => {
   return (
     <div>
-      <h1>Main Page</h1>
       <Link href="/about">
-        <a>About</a>
+        <button>About</button>
       </Link>
       <Link href="/contacts">
         <a>Contacts</a>
       </Link>
+      <h1>Main Page</h1>
     </div>
   );
 };
