e1e9f90f179c34bf9353d8b1fbd8b2ed9c15bcd7
Mikhail Shardin <kriptonit1988@gmail.com>
2019-08-10 17:09:33 +0500

    Компонент Link

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,15 @@
+import Link from "next/link";
+
 const Blablabla = () => {
-  console.log('index');
   return (
     <div>
       <h1>Main Page</h1>
+      <Link href="/about">
+        <a>About</a>
+      </Link>
+      <Link href="/contacts">
+        <a>Contacts</a>
+      </Link>
     </div>
   );
 };
