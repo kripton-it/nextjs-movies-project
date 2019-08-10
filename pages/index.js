392c39c31ee2ea5dfe25e7dd32273371465123d0
Mikhail Shardin <kriptonit1988@gmail.com>
2019-08-10 20:04:22 +0500

    Оптимальный запрос данных в классовом и функциональном компонентах

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
-// название - любое
-const Blablabla = () => {
+// import fetch from 'isomorphic-unfetch';
+
+const Index = () => {
   return (
     <div>
       <h1>Main Page</h1>
@@ -7,5 +8,44 @@ const Blablabla = () => {
   );
 };
 
-// ОБЯЗАТЕЛЬНО!!!!! НУЖЕН!!! default!!!
-export default Blablabla;
+Index.getInitialProps = async ({ req }) => {
+  console.log("fetching");
+  // const res = await fetch('https://api.github.com/repos/zeit/next.js');
+  // const json = await res.json();
+  // return { stars: json.stargazers_count };
+};
+
+export default Index;
+
+/* import React, { Component } from "react";
+
+class Index extends Component {
+  constructor(props) {
+    super(props);
+    // сработает и на сервере, и на клиенте
+    // console.log("fetching");
+  }
+
+  // сработает ТОЛЬКО на клиенте
+  // componentDidMount() {
+  //   console.log("fetching");
+  // }
+
+  static async getInitialProps({ req }) {
+    // При первом рендере сработает только на сервере
+    // При последующих - только на клиенте
+    console.log("fetching");
+    // const userAgent = req ? req.headers['user-agent'] : navigator.userAgent;
+    // return { userAgent };
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Main Page</h1>
+      </div>
+    );
+  }
+}
+
+export default Index;*/
