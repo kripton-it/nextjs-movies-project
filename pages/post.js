6cd1d60d28f3b42ea11099825dd43ec316fb26ce
Mikhail Shardin <kriptonit1988@gmail.com>
2019-08-11 12:25:10 +0500

    Альтернатива с помощью withRouter

--- pages/post.js
+++ pages/post.js
@@ -1,8 +1,16 @@
-const Post = ({id}) => (
-  <h1>Post Page № {id}</h1>
-)
+const Post = ({ id }) => <h1>Post Page № {id}</h1>;
 
 // query = { id: "..." }
 Post.getInitialProps = async ({query}) => query;
 
 export default Post;
+
+/* Второй вариант
+
+import { withRouter } from "next/router";
+
+const Post = ({ router }) => <h1>Post Page № {router.query.id}</h1>;
+
+export default withRouter(Post); */
+
+
